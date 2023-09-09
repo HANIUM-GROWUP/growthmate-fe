7bbc401d8890a232f5f09a47fbef7b621588c6fa
dhdld <btj5420@naver.com>
2023-09-09 15:14:01 +0900

    git

--- screens/viewPost.js
+++ screens/viewPost.js
@@ -90,7 +90,7 @@ const [loading, setLoading] = useState(false);
 
 const getData = () => {
   setLoading(true);
-  fetch("http://jsonplaceholder.typicode.com/comments")
+  fetch("https://jsonplaceholder.typicode.com/comments")
     .then((res) => res.json())
     .then((res) => setData(commentData.concat(res.slice(offset, offset + LIMIT))))
     .then(() => {
